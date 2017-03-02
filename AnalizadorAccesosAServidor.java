ff79a65d858719abe6ec41927db1cd3992c54437
Óscar Fernández <oscar.fer.rodz@gmail.com>
2017-03-02 11:51:44 +0100

    actividad 0655 Pasa los test

--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -3,7 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
-public class AnalizadorAccesosAlServidor
+public class AnalizadorAccesosAServidor
 {
     // instance variables - replace the example below with your own
     private ArrayList<Acceso> accesos;
@@ -12,7 +12,7 @@ public class AnalizadorAccesosAlServidor
     /**
      * Constructor for objects of class AnalizadorAccesosAlServidor
      */
-    public AnalizadorAccesosAlServidor()
+    public AnalizadorAccesosAServidor()
     {
         accesos = new ArrayList<Acceso>();
         enteros = new ArrayList<Integer>();
@@ -21,6 +21,8 @@ public class AnalizadorAccesosAlServidor
 
     public void analizarArchivoDeLog(String nombreArchivo){
         try{
+            enteros.clear();
+            accesos.clear();
             File archivo = new File(nombreArchivo);
             Scanner sc = new Scanner(archivo);
             int contador = 0;
@@ -29,8 +31,9 @@ public class AnalizadorAccesosAlServidor
                 contador++;
             }
             while(sc.hasNextLine()){
-                System.out.println(sc.nextLine());
-                String[] cadenaAPartir = sc.nextLine().split(" ");
+                String lineaLeer = sc.nextLine();
+                System.out.println(lineaLeer);
+                String[] cadenaAPartir = lineaLeer.split(" ");
                 int año = Integer.parseInt(cadenaAPartir[0]);
                 int mes = Integer.parseInt(cadenaAPartir[1]);
                 int dia =Integer.parseInt(cadenaAPartir[2]);
@@ -38,8 +41,9 @@ public class AnalizadorAccesosAlServidor
                 int minuto =Integer.parseInt(cadenaAPartir[4]);
                 Acceso nuevoAcceso = new Acceso(año,mes,dia,hora,minuto);
                 accesos.add(nuevoAcceso);
-                enteros.set(hora ,enteros.get(hora) + 1);
+                enteros.set(hora ,enteros.get(hora) + 1);    //El problema creo que está aquí
             }
+
         }
         catch(FileNotFoundException e){
             e.printStackTrace();
@@ -54,7 +58,7 @@ public class AnalizadorAccesosAlServidor
         int posicionMaxima = -1;
         horaConMasAccesos = -1;
         for(int contador = 0;contador < enteros.size();contador++){
-            if(enteros.get(contador) > horaConMasAccesos){
+            if(enteros.get(contador) >= horaConMasAccesos){
                 horaConMasAccesos = enteros.get(contador);
                 posicionMaxima = contador;
             }
